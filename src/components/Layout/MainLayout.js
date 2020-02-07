caeacb6839b6c1236fcdca9644d842313aad6813
youssefwork <youssef.samih97@gmail.com>
2020-02-08 00:16:07 +0100

    add collapse animation to sidebar and segment page

--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -9,13 +9,20 @@ export default class MainLayout extends Component {
     visible: true,
     dimmed: true
   }
+
+  disableVisible = () => {
+    this.setState({
+      visible: !this.state.visible
+    })
+  };
+
   render() {
     const { children } = this.props;
     return (
       <main>
           <Sidebar.Pushable as={Segment} >
-            <Sidebare visible={this.state.visible} />
-            <Sidebar.Pusher>
+            <Sidebare visible={this.state.visible} disableVisible={this.disableVisible} />
+            <Sidebar.Pusher style={{transform: !this.state.visible ? 'translate3d(90px,0,0)' : 'translate3d(150px,0,0)'}} >
               <Segment basic>
                 <Content fluid>
                   {children}
--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -13,30 +13,33 @@ class Sidebare extends Component { render() {
           vertical
           visible={this.props.visible}
           width='thin'
+          style={{ transition: 'width 0.25s', width: this.props.visible ? 150 : 93 }}
+          className="visible"
         >
           <Menu.Item as='a' className="logoContainer">
-            <Header as="h1" textAlign="center" className='logo' >
+            <Header as="h1" textAlign="center" className='logo' style={{ width: this.props.visible ? '85%' : '60%'}} >
               <span>S</span><span>B</span>
             </Header>
+            <p>{this.props.visible ? 'Avocat' : ''}</p>
           </Menu.Item>
-          <Menu.Item as='a'>
+          <Menu.Item as='a' onClick={() => this.props.disableVisible()} >
             <Icon name="list" style={{ color: "#3C3B4A" }} />
           </Menu.Item>
           <Menu.Item as='a'>
             <Icon name='home' style={{ color: "#3C3B4A" }} />
-            Accueil
+            {this.props.visible ? 'Accueil' : ''}
           </Menu.Item>
           <Menu.Item as='a'>
             <Icon name='address card outline' style={{ color: "#3C3B4A" }} />
-            Présentation
+            {this.props.visible ? 'Présentation' : ''}
           </Menu.Item>
           <Menu.Item as='a'>
             <Icon name='balance scale' style={{ color: "#3C3B4A" }} />
-            Expertises
+            {this.props.visible ? 'Expertises' : ''}
           </Menu.Item>
           <Menu.Item as='a' className="contactborder">
             <Icon name='phone square' style={{ color: "#3C3B4A" }} />
-            Contact
+            {this.props.visible ? 'Contact' : ''}
           </Menu.Item>
         </Sidebar>
         <Menu.Item className="twentyHours">
